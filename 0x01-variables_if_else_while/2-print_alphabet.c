5dfb72a569c5119ac6497e047148bef9e4e03687
Sleek <esthercikechukwu@gmail.com>
2023-08-13 13:55:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -this is the entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for  (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
