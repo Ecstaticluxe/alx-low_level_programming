1d2fe69380b96e8459759be5244cb513b7d317d5
Sleek <esthercikechukwu@gmail.com>
2023-08-14 00:30:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - this is the entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
