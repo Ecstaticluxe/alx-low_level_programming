a84245edce30daffaaa0b182f8d1337193aecec3
Sleek <esthercikechukwu@gmail.com>
2023-08-14 12:12:36 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - this is the entry
+* Return: always 0 (success)
+*/
+int main(void)
+{
+char
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar
+}
+putchar('n/');
+return (0)
+}
