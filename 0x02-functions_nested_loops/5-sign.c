92d506dc903e4d6510e201814982981453df083f
Sleek <esthercikechukwu@gmail.com>
2023-08-15 20:47:28 -0700

    5 sign' '

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_sign - check if the number is greater, less than or equal to zero
+  * @n: the input is an integer
+  * Return: 1 is greater than 0 or 0 is zero
+  */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar (43);
+return (1);
+}
+else if (n <0)
+{
+_putchar (45);
+return (-1);
+}
+else
+{
+_putchar (48);
+return (0);
+}
+}
