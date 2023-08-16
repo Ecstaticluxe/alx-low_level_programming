24f148de0959fc4f978664f8f39b10122bfb9191
Sleek <esthercikechukwu@gmail.com>
2023-08-15 19:49:09 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_alphabbet - this is the entry point of the alphabet
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 1; i <= 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
