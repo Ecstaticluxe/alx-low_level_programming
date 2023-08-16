3a3df871f44a53317ff33a18c6b698b9b6137fd1
Sleek <esthercikechukwu@gmail.com>
2023-08-15 20:07:56 -0700

    alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  *print_alphabbet - making the alphabets 10 times
+  *print_alphabet_x10 - making the alphabets 10 times
   * Return: void
   */
 void print_alphabet_x10(void)
