9394988cfd9d487802983719a74a45f21d3a1ddd
Sleek <esthercikechukwu@gmail.com>
2023-08-21 21:04:01 -0700

    7puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * puts_half - prints half of a string
- * @str: half of the string to be printed 
+ * @str: half of the string to be printed
  * Return: 0
  */
 void puts_half(char *str)
 {
 	int i, n, count = 0;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+for (i = 0 ; str[i] != '\0' ; i++)
 	count++;
 	n = (count - 1) / 2;
 	for (i = n + 1 ; str[i] != '\0' ; i++)
