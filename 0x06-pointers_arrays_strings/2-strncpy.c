2768108a353c6a644372f3aca34b196d32c2d3ce
Sleek <esthercikechukwu@gmail.com>
2023-08-24 20:18:37 -0700

    2strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates two strings
+ * @dest: return value
+ * @src: input value
+ * @n: number of byte
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+while (j < n && src[j] != '\0')
+{
+	dest[j] = src[j];
+	j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
