6321450800dc160d4d3a2d47fc31c753aa4f536e
Sleek <esthercikechukwu@gmail.com>
2023-08-29 14:23:17 -0700

    :5-strstr git push

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,23 @@
 #include "main.h"
 /**
- *
- *
- * Returun: pointer
+ * _strstr - locates a substring.
+ * @haystack: input
+ * @needle: input
+ * Return: pointer
  */
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 {
-
+	for(; *haystack != '\0'; haystack++)
+{
+char *one = haystack;
+char *two = needle;
+while (*one == *two && *two != '\0')
+{
+one++;
+two++;
+}
+if (*two == '\0')
+return (haystack);
+}
+return (0);
+}
