f9f609f179878cd944c8d9b26ba2249dad9573f8
Sleek <esthercikechukwu@gmail.com>
2023-08-30 17:28:17 -0700

    all

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_palindrome - returns a string
+ * @s: string
+ * Return 1
+ */
+int is_palindrome(char *s)
+{
+if (* == 0)
+return (is_palindrome(s, 0, strlen_recursion(s)));
+if (*s == '\n')
+return (0);
+return (i + strlen s + 1)
+}
