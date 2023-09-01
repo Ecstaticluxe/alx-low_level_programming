2e417d45f12236436344b361eabb1a39cfa416d0
Sleeky <esthercikechukwu@gmail.com>
2023-09-01 20:31:16 +0000

    2-args.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: array of argument
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: array of argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv; /* ignore argv*/
+printf("$d\n", argc - 1);
+return (0);
+}
