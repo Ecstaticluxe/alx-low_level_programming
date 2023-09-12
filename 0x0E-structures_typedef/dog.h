15e14d2adf04624db87a8bb66b4a5417c719a5f6
Sleeky <esthercikechukwu@gmail.com>
2023-09-12 21:14:11 +0000

    all

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - element type
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+		float age;
+		char *owner;
+};
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
