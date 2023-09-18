037ad71d3f136a4ff37819904206789aa370a4b4
Sleeky <esthercikechukwu@gmail.com>
2023-09-18 20:46:10 +0000

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - check if a tring contains a char
+ * @s: string
+ * Return: 0
+ */
+int is_digit(char *s)
+{
+
+	int i = 0;
+
+	while
