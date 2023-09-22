0f630953e66be3fec8ba38a056fba2bdc8009f05
Sleeky <esthercikechukwu@gmail.com>
2023-09-22 04:18:04 +0000

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,13 +10,13 @@
  */
 void free_list(list_t *head)
 {
-list_t *current;
 
-while ((current = head) != NULL)
-{
-	head = head->next;
-	free(current->str);
-	head = (current);
+list_t *temp;
+while (head)
+	{
+	temp = head->next;
+	free(head->str);
+	free(head);
+	head = temp;
+	}
 }
-}
-
