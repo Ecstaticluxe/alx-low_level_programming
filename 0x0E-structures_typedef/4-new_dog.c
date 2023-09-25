ae687472898458c023ca795cede65e936c94c476
Sleeky <esthercikechukwu@gmail.com>
2023-09-25 14:21:08 +0000

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,11 +16,11 @@ dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *str)
 {
 
-        int len = 0;
+int len = 0;
 
-        while (*str++)
-                len++;
-        return (len);
+while (*str++)
+len++;
+return (len);
 }
 
 /**
