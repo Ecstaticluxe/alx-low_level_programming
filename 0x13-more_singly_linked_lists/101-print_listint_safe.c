2b8bc17366d71061e12818fff99b17fa094cf2b0
Sleeky <esthercikechukwu@gmail.com>
2023-09-26 18:20:54 +0000

    13. Free (safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint -  a function that prints a listint_t linked list
+ * @head: pointer
+ * Returns: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head);
+
+
