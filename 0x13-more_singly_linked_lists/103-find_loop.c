31cbfc2952a276c8af0cf261b38a769ec8aabb2d
Sleeky <esthercikechukwu@gmail.com>
2023-09-27 21:39:57 +0000

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -12,4 +12,27 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 
+listint_t *p = head;
+	listint_t *k = head;
+
+	if (!head)
+		return (NULL);
+
+	while (p && k && k->next)
+	{
+		k = k->next->next;
+		p = p->next;
+		if (k == p)
+		{
+			p = head;
+			while (p != k)
+			{
+				p = p->next;
+				k = k->next;
+			}
+			return (k);
+		}
+	}
+
+	return (NULL);
 }
