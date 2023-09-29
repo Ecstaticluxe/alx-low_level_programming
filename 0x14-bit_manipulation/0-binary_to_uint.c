9a3fd5a8d17a10bd47f3ae5bcdaed2b431265ded
Sleeky <esthercikechukwu@gmail.com>
2023-09-29 18:25:14 +0000

    100-get_endianness.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,27 +8,36 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int i;
-int len, base_two;
+unsigned int i, j, len, power, sum;
+int base;
 
-if (!b)
-	return (0);
-
-i = 0;
-
-for (len = 0; b[len] != '\0'; len++)
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+base = 2;
+sum = 0;
+power = 1;
 	{
-		if (b[len] != '0' && b[len] != '1')
+	if (b == NULL)
+		return (0);
+	}
 		{
-			return (0);
+			for (len = 0; b[len] != '\0'; len++)
+				;
+if (len == 1 && (b[0] == '0' || b[0] == '1');
+		return (b[0] - 48)
 		}
-if (b[len] & 1)
+	{
+	for (i = 0; b[i] != '\0'; i++)
+	;
+
+	if (b[i] != '0' && b[i] != '1')
+	return (0);
+	}
 		{
-i = base_two;
-	}
-	}
-return (i);
+		for (j = len - 1; j > 0; j--)
+		power = power *base;
+		sum = sum + (power * (b[i] - 48));
+		len--;
+		power = 1;
+		}
+		return (sum);
 
 }
