ffe916d12d3b5c99c39bb24a745054096aae6835
Esther <esthercikechukwu@gmail.com>
2023-09-30 09:29:04 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,36 +8,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int i, j, len, power, sum;
-int base;
+int i;
+unsigned int decimal;
 
-base = 2;
-sum = 0;
-power = 1;
-	{
-	if (b == NULL)
-		return (0);
-	}
-		{
-			for (len = 0; b[len] != '\0'; len++)
-				;
-if (len == 1 && (b[0] == '0' || b[0] == '1');
-		return (b[0] - 48)
-		}
-	{
-	for (i = 0; b[i] != '\0'; i++)
-	;
+if (!b)
+return (0);
 
-	if (b[i] != '0' && b[i] != '1')
-	return (0);
-	}
-		{
-		for (j = len - 1; j > 0; j--)
-		power = power *base;
-		sum = sum + (power * (b[i] - 48));
-		len--;
-		power = 1;
-		}
-		return (sum);
+for (i = 0; b[i]; i++)
 
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+decimal = 2 * decimal + (b[i] - '0');
+
+}
+
+return (decimal);
 }
