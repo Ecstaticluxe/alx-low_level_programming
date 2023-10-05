869fab913c8b39dedbe55eb5516de4104c9f1e7d
Sleeky <esthercikechukwu@gmail.com>
2023-10-05 05:14:42 +0000

    readelf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -175,11 +175,21 @@ void print_osabi(unsigned char *e_ident)
 		break;
 	case ELFOSABI_STANDALONE:
 		printf("Standalone App\n");
-break
+break;
+
 default:
 printf("Unknown:%x>\n", e_ident[EI_OSABI]);
-
+	}
 }
+/**
+ * print_abi - function that print abi version of elf header file
+ * @e_ident: a pointer
+ */
+
+void print_abi(unsigned char *e_ident)
+{
+	printf(" ABI Version: %d\n",
+			e_ident[EI_ABIVERSION]);
 
 }
 
@@ -256,6 +266,46 @@ void close_elf(int elf)
 		exit(98);
 	}
 }
+/**
+ * main - Displays the information contained in the
+ * ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the file is not an ELF File or
+ * the function fails - exit code 98.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	Elf64_Ehdr *header;
+	int o, r;
+
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+	r = read(o, header, sizeof(Elf64_Ehdr));
+	if (r == -1)
+	{
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
+	}
+
+
+
 
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
@@ -271,3 +321,4 @@ void close_elf(int elf)
 	free(header);
 	close_elf(o);
 	return (0);
+}
