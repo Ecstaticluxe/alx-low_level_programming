82ab537a706eafa640e435c815cfe9f011ddb673
Essy <esthercikechukwu@gmail.com>
2023-12-01 18:30:55 +0000

    mandatory

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,20 +8,44 @@
  * @ht: the hash table
  *
  */
-void hash_table_delete(hash_table_t *ht);
+
+typedef struct Node
+{
+    char *key;
+    int value;
+    struct Node *next;
+} Node;
+
+typedef struct
+{
+    size_t size;
+    Node **array;
+} hash_table_t;
+
+
+void hash_table_delete(hash_table_t *ht) 
 {
 	hash_node_t *bucket, *aux_free;
-	unsigned long int i = 0;
-
-	if (!ht)
-		return;
-
-	for (i = 0; i < ht->size; i++)
-	{
-		bucket = ht->array[i];
-		while (bucket)
+	        unsigned long int i = 0;
+		
+		if (ht == NULL)
 		{
-
-			aux_free = bucket;
-			bucket = bucket
-
+			return;
+		}
+		
+		for (size_t i = 0; i < ht->size; ++i)
+		{
+			Node *current = ht->array[i];
+			Node *next;
+			
+			while (current != NULL)
+	{
+		next = current->next;
+		free(current->key);
+		free(current);
+		current = next;
+	}
+		}
+		free(ht->array);
+		free(ht);
+}
